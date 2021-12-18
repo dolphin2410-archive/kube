44e8f7d972ab67818b1eb1199f1d73483836a950
dolphin2410 <dolphin2410@outlook.kr>
2021-12-18 14:54:07 +0900

    Update Structure

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -1,38 +1,63 @@
 use nfd::Response;
 use web_view::Content;
+use std::path::Path;
 
-pub fn start() {
-    let html_content = include_str!("../../resources/root.html");
-    let _target_zip = include_bytes!(include_str!("../../target.txt"));
+const _TARGET_ZIP: &[u8] = include_bytes!(concat!("../../", include_str!("../../target.txt")));
 
+
+pub fn root() -> &'static str {
+    include_str!("../../resources/root.html")
+}
+
+pub fn next_page() -> &'static str {
+    include_str!("../../resources/next_page.html")
+}
+
+pub fn render(html: &str) {
     web_view::builder()
         .title("kube")
-        .content(Content::Html(html_content))
+        .content(Content::Html(html))
         .size(800, 500)
         .resizable(false)
         .user_data(())
         .invoke_handler(|webview, arg| {
             match arg {
                 "hello" => {
-                    println!("Hello, World!")
+                    println!("Hello, World!");
                 },
                 "open_file" => {
                     let result = nfd::open_pick_folder(None).unwrap();
                     match result {
                         Response::Okay(path) => {
-                            webview.eval(&format!("update_folder(\"{}\")", path.replace("\\", "/"))).unwrap();
+                            webview.eval(&format!(r#"update_folder("{}")"#, path.replace("\\", "/"))).unwrap();
                         },
                         _ => {}
                     }
                 },
-                "next_page" => {
-                    todo!("Must do!")
+                _ => {
+                    if arg.starts_with("next_page:") {
+                        let path = arg.replace("next_page:", "");
+                        
+                        let exists = Path::new(&path.as_str()).exists();
+
+                        if !exists {
+                            webview.eval("no_file()").unwrap();
+                        } else {
+                            println!("WOW");
+                            webview.eval(&format!(r#"move_page("{}")"#, next_page())).unwrap();
+                        }
+                    }
                 }
-                _ => unimplemented!()
             };
 
             Ok(())
         })
+        .build()
+        .unwrap()
         .run()
         .unwrap();
+}
+
+pub fn start() {
+    render(root());
 }
