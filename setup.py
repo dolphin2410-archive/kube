5308826d69b8b20942a9b8ceb9937169864d4702
dolphin2410 <dolphin2410@outlook.kr>
2021-12-18 23:53:48 +0900

    Fix Python

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import sys
 import os
 import shutil
 from enum import Enum
+import subprocess
 
 # OS Type Enum
 class OS(Enum):
@@ -30,7 +31,7 @@ if shutil.which("git") is None:
 
 if shutil.which("cargo") is None:
     read = ""
-    while true:
+    while True:
         read = input("Rust is not installed. Install? [Y/n]")
         if read == "\n" or read.lower() == "y":
             break
@@ -40,25 +41,27 @@ if shutil.which("cargo") is None:
 
     # Install Rust
     if current_os == OS.LINUX or current_os == OS.MACOS:
-        os.popen("curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y")
+        subprocess.Popen(["curl", "--proto", "'=https'", "--tlsv1.2", "https://sh.rustup.rs", "-sSf", "|", "sh", "-s", "--", "-y"]).wait()
     else:
-        os.popen("curl -o installer.exe https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe")
-        os.popen("installer.exe -y")
+        subprocess.Popen(["curl", "-o", "installer.exe", "https://static.rust-lang.org/rustup/dist/x86_64-pc-windows-msvc/rustup-init.exe"])
+        subprocess.Popen(["installer.exe", "-y"]).wait()
         os.remove("installer.exe")
 
-os.popen("git clone https://github.com/dolphin2410/kube")
+subprocess.Popen(["git", "clone", "https://github.com/dolphin2410/kube"]).wait()
 shutil.copy("target.txt", "kube/target.txt")
 shutil.copy("app.txt", "kube/app.txt")
 f = open("target.txt", "r")
 zip_archive = f.read()
 shutil.copy(zip_archive, "kube/" + zip_archive)
 
-os.popen("cd kube && cargo build --release")
+subprocess.check_output(["cd", "kube", "&&", "cargo", "build", "--release"]).decode('utf-8')
+print("DONE _FHTISGHEO")
+strs = subprocess.check_output(['cd', 'kube', '&&', 'ls', '-la']).decode('utf-8')
+print(strs)
 if current_os == OS.WINDOWS:
-    shutil.copy("kube/target/releases/kube.exe", "installer.exe")
+    shutil.copy("kube/target/release/kube.exe", "installer.exe")
 else:
-    shutil.copy("kube/target/releases/kube", "installer")
+    shutil.copy("kube/target/release/kube", "installer")
 
 shutil.rmtree("kube")
 print("DONE!")
-
